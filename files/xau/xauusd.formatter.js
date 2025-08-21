45cd40df9bdcb0b57606a6dd0bedd1d290a5217d
lwpassvoice <zgdxst@qq.com>
2025-08-21 23:01:56 +0800

    data

--- files/xau/xauusd.formatter.js
+++ files/xau/xauusd.formatter.js
@@ -32,22 +32,40 @@ const fs = require("fs");
 const path = require("path");
 const data = require("./XAUUSD.data.json");
 
+// function formatData(rawData) {
+//   return rawData.map((item) => {
+//     const date = new Date(item.time * 1000 + 3 * 3600000);
+//     date.setUTCHours(0, 0, 0, 0);
+//     const dateStr = date.toLocaleString();
+//     return {
+//       time: item.time * 1000,
+//       open: item.open,
+//       close: item.close,
+//       high: item.max,
+//       low: item.min,
+//       volume: item.volume,
+//       date: dateStr,
+//     };
+//   });
+// }
+
 function formatData(rawData) {
   return rawData.map((item) => {
     const date = new Date(item.time * 1000 + 3 * 3600000);
     date.setUTCHours(0, 0, 0, 0);
     const dateStr = date.toLocaleString();
     return {
-      time: item.time * 1000,
+      x: date.getTime(),
+      y: item.close,
+      time: dateStr,
       open: item.open,
-      close: item.close,
       high: item.max,
       low: item.min,
       volume: item.volume,
-      date: dateStr,
     };
   });
 }
+
 const formattedData = formatData(data);
 // 写入文件
 fs.writeFileSync(
